3c9b1a34ee6a501c73508de9b2fe79c4a6cad084
p.reutov <reutov@caweb.ru>
2022-09-21 10:49:52 +0800

    learn 11

--- README.md
+++ README.md
@@ -34,4 +34,5 @@ The easiest way to deploy your Next.js app is to use the [Vercel Platform](https
 Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
 
 ## Заметки
-
+Плэйлист:
+https://www.youtube.com/playlist?list=PLC3y8-rFHvwgC9mj0qv972IO5DmD-H0ZH
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,21 @@
 import Link from "next/link";
+import {useRouter} from "next/router";
 
 function Home() {
+    const router = useRouter()
+
+    const handleClick = () => {
+        console.log('placing your router')
+        router.push('/product')
+    }
     return (
         <div>
             <h1>Home page</h1>
             <Link href={'/blog'}><a>Blog</a></Link>
             <Link href={'/product'}><a>Product</a></Link>
+            <button onClick={handleClick}>
+                Place Order
+            </button>
         </div>
     )
 }
