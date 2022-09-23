11f02c47cb6e288df608389cb9603958b554b2b5
CaWeb007 <spcell007@gmail.com>
2022-09-23 18:26:49 +0800

    23

--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -9,6 +9,17 @@ const Post = ({post}) => {
 
 export default Post
 
+export async function getStaticPaths() {
+    return {
+        paths: [
+            {params : {postId: '1'}},
+            {params : {postId: '2'}},
+            {params : {postId: '3'}}
+        ],
+        fallback: false
+    }
+}
+
 export async function getStaticProps(context){
     const {params} = context
     const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`)
