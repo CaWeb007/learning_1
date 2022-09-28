b5d8ffc50289a3267d7d5e66a6570012e8c72c32
CaWeb007 <spcell007@gmail.com>
2022-09-28 17:11:58 +0800

    fallback false

--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -12,20 +12,26 @@ export default Post
 export async function getStaticPaths() {
     const response = await fetch('https://jsonplaceholder.typicode.com/posts')
     const data = await response.json()
-    // let paths = []
-    // data.map(post => {
-    //     paths.push({params : {postId: String(post.id)}})
-    // })
-    const paths = data.map(post => {
+    /*const paths = data.map(post => {
+        if (post.id > 3) return
         return {
             params: {
                 postId: `${post.id}`
             }
         }
-    })
+    })*/
+    let paths = [];
+    for (let i = 0; i < data.length; i++){
+        if (data[i].id > 3) break
+        paths.push({
+            params: {
+                postId: String(data[i].id)
+            }
+        })
+    }
     return {
         paths,
-        fallback: false
+        fallback: false // false, true, 'blocking'
     }
 }
 
