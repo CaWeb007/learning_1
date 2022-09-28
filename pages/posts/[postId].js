b00baaa70db60483fdb6354ca59272890fa7e387
CaWeb007 <spcell007@gmail.com>
2022-09-28 16:20:21 +0800

    static generation dynamic posts

--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -1,7 +1,7 @@
 const Post = ({post}) => {
     return (
         <>
-            <h2>{post.title}</h2>
+            <h2>{post.id}: {post.title}</h2>
             <p>{post.body}</p>
         </>
     );
@@ -10,12 +10,21 @@ const Post = ({post}) => {
 export default Post
 
 export async function getStaticPaths() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+    const data = await response.json()
+    // let paths = []
+    // data.map(post => {
+    //     paths.push({params : {postId: String(post.id)}})
+    // })
+    const paths = data.map(post => {
+        return {
+            params: {
+                postId: `${post.id}`
+            }
+        }
+    })
     return {
-        paths: [
-            {params : {postId: '1'}},
-            {params : {postId: '2'}},
-            {params : {postId: '3'}}
-        ],
+        paths,
         fallback: false
     }
 }
