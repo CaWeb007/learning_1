302ca84deca5e75af220e18f1270b19edb2159df
CaWeb007 <spcell007@gmail.com>
2022-10-06 12:05:30 +0800

    events list with category filter

--- pages/events/index.js
+++ pages/events/index.js
@@ -2,20 +2,38 @@ import Link from "next/link";
 import {useState} from "react";
 import {useRouter} from "next/router";
 
-const EventList = ({eventList}) => {
+const EventList = ({eventList, categoryList}) => {
     const [events, setEvents] = useState(eventList)
+    const [category, setCategory] = useState(categoryList)
     const router = useRouter();
-    const fetchFilterEvents = async () => {
-        const response = await fetch('http://localhost:4000/events?category=sport')
+    const fetchFilterEvents = async (event) => {
+        const queryString = (event.target.value) ? `category=${event.target.value}` : ''
+        const response = await fetch(`http://localhost:4000/events?${queryString}`)
         const data = await response.json()
         setEvents(data)
-        router.push('/events?category=sport', undefined, {shallow: true})
+        router.push(`/events?${queryString}`, undefined, {shallow: true})
+        const category = []
+        if (queryString === '')
+            data.map(item => {
+                if (category.find(element => element === item.category) === undefined)
+                    category.push(item.category)
+            })
+        setCategory(category)
     }
     return(
         <>
             <h1>Events List</h1>
             <div>
-                <button onClick={fetchFilterEvents}>Show sports</button>
+                {category.map(item => {
+                    return(
+                        <div key={item}>
+                            <button value={item} onClick={fetchFilterEvents}>Show {item}</button>
+                        </div>
+                    )
+                })}
+                <div key={'all'}>
+                    <button value={''} onClick={fetchFilterEvents}>Show all</button>
+                </div>
             </div>
             <div>
                 {events.map(item => {
@@ -36,12 +54,20 @@ const EventList = ({eventList}) => {
 }
 export default EventList
 export async function getServerSideProps({query}){
-    const queryString = (query.category) ? 'category=sport' : ''
+    const queryString = (query.category) ? `category=${query.category}` : ''
     const response = await fetch(`http://localhost:4000/events?${queryString}`)
     const data = await response.json()
+    const category = []
+    if (queryString === '')
+        data.map(item => {
+            if (category.find(element => element === item.category) === undefined)
+                category.push(item.category)
+        })
+
     return {
         props: {
-            eventList: data
+            eventList: data,
+            categoryList: category
         }
     }
 }
