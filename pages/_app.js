963ca9775032fed3da966a0ebbbeb406821f7292
CaWeb007 <spcell007@gmail.com>
2022-10-12 11:21:04 +0800

    import settings

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
-import '../styles/globals.sass'
+import 'styles/globals.sass'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Header from "../components/header"
-import Footer from "../components/footer"
+import Header from "components/header"
+import Footer from "components/footer"
 import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
