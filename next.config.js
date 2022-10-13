6febe0b165c7d2c91ed7e437de8f828aea81c394
CaWeb007 <spcell007@gmail.com>
2022-10-13 12:42:03 +0800

    redirect

--- next.config.js
+++ next.config.js
@@ -2,6 +2,26 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  redirects: async () => {
+    return [
+      {
+        source: '/blog/first',
+        destination: '/blog',
+        //permanent: true//308 Permanent redirect
+        permanent: false//307 Temporary redirect
+      },
+      {
+        source: '/articles',
+        destination: '/news',
+        permanent: true
+      },
+      {
+        source: '/articles/:category/:newsId',
+        destination: '/news/:category/:newsId',
+        permanent: true
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,4 +1,15 @@
+import {useRouter} from "next/router";
+
 function Blog() {
-    return <h1>Blog page</h1>
+    const router = useRouter()
+    function testRedirect() {
+        router.push("/blog/first")
+    }
+    return (
+        <>
+            <h1>Blog page</h1><br/>
+            <button className='btn btn-success' onClick={testRedirect}>Go to '/blog/first'</button>
+        </>
+    )
 }
 export default Blog
