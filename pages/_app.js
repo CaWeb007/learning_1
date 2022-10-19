6ae8f3b25cafbe743630d0523e46a46b17d7685f
CaWeb007 <spcell007@gmail.com>
2022-10-19 17:16:18 +0800

    next auth Provider передает session в компоненты и при переходах

--- pages/_app.js
+++ pages/_app.js
@@ -4,20 +4,23 @@ import Header from "components/header"
 import Footer from "components/footer"
 import Head from "next/head";
 import {Container} from "react-bootstrap";
+import {Provider} from "next-auth/client";
 
 function MyApp({ Component, pageProps }) {
     if (Component.getLayout)
         return Component.getLayout(<Component {...pageProps} />)
     return (
-        <Container>
-            <Head>
-                <title>Default title</title>
-                <link rel="shortcut icon" href="/favicon.ico" />
-            </Head>
-            <Header/>
-            <Component {...pageProps} />
-            <Footer/>
-        </Container>
+        <Provider>
+            <Container>
+                <Head>
+                    <title>Default title</title>
+                    <link rel="shortcut icon" href="/favicon.ico" />
+                </Head>
+                <Header/>
+                <Component {...pageProps} />
+                <Footer/>
+            </Container>
+        </Provider>
     )
 }
 
